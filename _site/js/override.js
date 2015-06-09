a566fe824ad3f2b76d9be426f9f031b3eab38b7a
Angus TC Pearson <angusp@tardis.ed.ac.uk>
2015-06-09 23:55:30 +0100

    moved hero content around, captions now 50% height

--- _site/js/override.js
+++ _site/js/override.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
 	$(window).scroll(function() {
-		if($(this).scrollTop() > ($(this).height() - 50)){
+		if($(this).scrollTop() > ($('.hero').height() - 50)){
 			$('#site-nav').addClass('woosh');
 		} else {
 			$('#site-nav').removeClass('woosh');
