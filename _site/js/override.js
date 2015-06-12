7bdbf5693ee22becfa23f755e06c58f547ba97ec
Angus TC Pearson <angusp@tardis.ed.ac.uk>
2015-06-12 23:37:41 +0100

    Production ready, added some (slightly waffled) 'news'

--- _site/js/override.js
+++ _site/js/override.js
@@ -1,10 +1,18 @@
 
 /* Smoot scroll to name anchors */
-$('a').click(function(){
-	$('html, body').animate({
-		scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-	}, 500);
-	return false;
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
 
 
